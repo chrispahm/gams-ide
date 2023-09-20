5b43e280b46cb0b502b0f81b33193f5db6e8a54c
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2023-09-20 11:32:45 +0200

    📦 package using webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const config = {
+  target: 'node',
+
+  entry: './extension.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'gams-ide.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
+    extensions: ['.ts', '.js'],
+    alias: {
+      // provides alternate implementation for node module and source files
+    },
+    fallback: {
+      // Webpack 5 no longer polyfills Node.js core modules automatically.
+      // see https://webpack.js.org/configuration/resolve/#resolvefallback
+      // for the list of Node.js core module polyfills.
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
+module.exports = config;
