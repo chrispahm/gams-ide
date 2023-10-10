7f75eba870dd2242f761b3aeb3bbf8bd484f32c7
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2023-10-10 22:35:25 +0200

    :tshirt: apply new lint rule to alle files

--- src/createRefTreeWithSymbolValues.js
+++ src/createRefTreeWithSymbolValues.js
@@ -1,9 +1,9 @@
-const vscode = require('vscode')
-const readline = require('readline')
-const fs = require('fs')
-const _ = require('lodash')
-const shell = require('shelljs')
-const path = require('path')
+const vscode = require('vscode');
+const readline = require('readline');
+const fs = require('fs');
+const _ = require('lodash');
+const shell = require('shelljs');
+const path = require('path');
 
 module.exports = async function createRefTreeWithSymbolValues(options) {
   const {
@@ -32,200 +32,199 @@ module.exports = async function createRefTreeWithSymbolValues(options) {
   try {
     await getData(lst, solves, solvesStore, referenceTree);
   } catch (error) {
-    throw error
+    throw error;
   }
   
   // save new data in store
   state.update("solves", solvesStore);
   state.update("referenceTree", referenceTree);
-}
+};
 
 function parseDMP(file, config, referenceTree) {
   return new Promise((resolve, reject) => {
-    const stream = fs.createReadStream(file)
+    const stream = fs.createReadStream(file);
     const rl = readline.createInterface({
       input: stream,
       crlfDelay: Infinity
-    })
+    });
 
-    let lineno = 0
-    const defaultSettings = vscode.workspace.getConfiguration("gamsIde")
-    const dumpFile = fs.createWriteStream(`${config.scratchdir}${path.sep}${path.basename(file, '.dmp')}.gms`, { flags: 'w' })
-    const symbols = _.filter(referenceTree, o => { return o.type === 'SET' || o.type === 'PARAM' && o.name })
-    const solves = []
+    let lineno = 0;
+    const defaultSettings = vscode.workspace.getConfiguration("gamsIde");
+    const dumpFile = fs.createWriteStream(`${config.scratchdir}${path.sep}${path.basename(file, '.dmp')}.gms`, { flags: 'w' });
+    const symbols = _.filter(referenceTree, o => { return o.type === 'SET' || o.type === 'PARAM' && o.name; });
+    const solves = [];
 
     rl.on('line', (line) => {
       if (/solve (.*?) using/i.test(line)) {
-        const model = line.split(/solve/i)[1].split(/\s+/)[1]
-        dumpFile.write('option dispWidth=15;\ndisplay\n')
-        lineno += 2
-        const display = lineno + 1
+        const model = line.split(/solve/i)[1].split(/\s+/)[1];
+        dumpFile.write('option dispWidth=15;\ndisplay\n');
+        lineno += 2;
+        const display = lineno + 1;
         symbols.forEach((symbol) => {
-          dumpFile.write(`$if defined ${symbol.name} ${symbol.name} \n`)
-          lineno++
-        })
+          dumpFile.write(`$if defined ${symbol.name} ${symbol.name} \n`);
+          lineno++;
+        });
 
-        dumpFile.write(';\n')
+        dumpFile.write(';\n');
         dumpFile.write(`${model}.limrow=${defaultSettings.get('consoleLimrow')};
           ${model}.limcol=${defaultSettings.get('consoleLimcol')};
           ${model}.solprint=1;
           ${model}.resLim=0;
           option limrow=${defaultSettings.get('consoleLimrow')};
           option limcol=${defaultSettings.get('consoleLimcol')};
-          `)
-        dumpFile.write(line + '\n')
-        lineno += 8
+          `);
+        dumpFile.write(line + '\n');
+        lineno += 8;
 
         solves.push({
           model: model,
           line: lineno,
           display: display
-        })
+        });
 
       } else if (!/display.*?;/gi.test(line)) {
-        lineno++
-        dumpFile.write(line + '\n')
+        lineno++;
+        dumpFile.write(line + '\n');
       }
-    })
+    });
 
     rl.on('close', () => {
-      dumpFile.end()
-      // fs.unlink(file, (err) => {
-      //   if (err) throw err
-      // })
+      dumpFile.write('$show');
+      dumpFile.end();
+      fs.unlink(file, (err) => {
+        if (err) throw err;
+      });
       resolve({
         dumpFile,
         solves
-      })
-    })
+      });
+    });
 
-    rl.on('error', reject)
-  })
+    rl.on('error', reject);
+  });
 }
 
 function execDMP(dumpFile, config) {
-  return new Promise((resolve, reject) => {
-    const gamsParams = config.gamsexe + ' "' + dumpFile.path + '" suppress=1 pageWidth=80 pageSize=0 lo=3 resLim=0 -scrdir="' + config.scratchdir + '" '
-    shell.cd(config.scratchdir)
+  return new Promise((resolve) => {
+    const gamsParams = config.gamsexe + ' "' + dumpFile.path + '" suppress=1 pageWidth=80 pageSize=0 lo=3 resLim=0 -scrdir="' + config.scratchdir + '" ';
+    shell.cd(config.scratchdir);
     
     shell.exec(gamsParams, {silent: true}, (code, stdout, stderr) => {
       if (code !== 0) {
-        console.log('Error in dmp exec: ' + stdout, stderr)
-        // reject(stderr)
+        console.warn('Error in dmp exec: ' + stdout, stderr);
       }
       fs.unlink(dumpFile.path, (err) => {
-        if (err) throw err
-      })      
-      resolve(`${config.scratchdir}${path.sep}${path.basename(dumpFile.path, '.gms')}.lst`)
-    })
+        if (err) throw err;
+      });      
+      resolve(`${config.scratchdir}${path.sep}${path.basename(dumpFile.path, '.gms')}.lst`);
+    });
 
-  })
+  });
 }
 
 function getData(lst, solves, gamsSolves, referenceTree) {
   return new Promise((resolve, reject) => {
-    const stream = fs.createReadStream(lst)
+    const stream = fs.createReadStream(lst);
     const rl = readline.createInterface({
       input: stream,
       crlfDelay: Infinity
-    })
-    let curSym, curData, curSection, curSolve, foundError
+    });
+    let curSym, curData, curSection, curSolve, foundError;
 
     function save(solve, symbol, data) {
       // save solve
-      const curSolve = _.find(solves, { 'line': Number(solve) })
+      const curSolve = _.find(solves, { 'line': Number(solve) });
       if (curSolve) {
         const statement = {
           model: curSolve.model,
           line: Number(solve)
-        }
+        };
         if (!_.find(gamsSolves, { 'line': Number(solve) })) {
-          gamsSolves.push(statement)
+          gamsSolves.push(statement);
         }
       }
       // save entry, overwrite any previous entries
       const entry = referenceTree.find(el => {
-        if (el.name && symbol) return el.name.toLowerCase() === symbol.toLowerCase()
-      })
+        if (el.name && symbol) return el.name.toLowerCase() === symbol.toLowerCase();
+      });
 
       if (entry) {
-        if (!entry.data) entry.data = {}
-        entry.data[`line_${Number(solve)}`] = data
-        curSym = ''
-        curData = ''
+        if (!entry.data) entry.data = {};
+        entry.data[`line_${Number(solve)}`] = data;
+        curSym = '';
+        curData = '';
       }
     }
 
     rl.on('line', (line) => {
       // Save current section of the listing file and the associated solve
       if (line.includes('E x e c u t i o n')) {
-        curSection = 'Displays'
+        curSection = 'Displays';
       } else if (line.includes('Equation Listing')) {
-        curSection = 'Equations'
-        curSolve = line.split(/[\s]+/)[8]
+        curSection = 'Equations';
+        curSolve = line.split(/[\s]+/)[8];
       } else if (line.includes('Column Listing')) {
-        curSection = 'Variables'
-        curSolve = line.split(/[\s]+/)[8]
+        curSection = 'Variables';
+        curSolve = line.split(/[\s]+/)[8];
       }
       // save data according to sections
       // Displays, Equations, Variables
       else if (/^(----)\s*/.test(line)) {
-        if (curSym) save(curSolve, curSym, curData)
+        if (curSym) save(curSolve, curSym, curData);
         if (curSection === 'Displays') {
-          const dispLine = Number(line.split(/[\s]+/)[1])
-          const symbol = line.split(/[\s]+/)[3]
-          const pieces = line.split(/[\s]+/)
-          pieces.splice(0, 2)
-          const data = pieces.join(' ')
-          const solve = _.find(solves, { 'display': dispLine })
-          if (!solve) return
-          curSym = symbol
-          curSolve = solve.line
-          curData = `---- ${data}`
+          const dispLine = Number(line.split(/[\s]+/)[1]);
+          const symbol = line.split(/[\s]+/)[3];
+          const pieces = line.split(/[\s]+/);
+          pieces.splice(0, 2);
+          const data = pieces.join(' ');
+          const solve = _.find(solves, { 'display': dispLine });
+          if (!solve) return;
+          curSym = symbol;
+          curSolve = solve.line;
+          curData = `---- ${data}`;
         } else {
-          const symbol = line.split(/[\s]+/)[1]
-          curSym = symbol
-          curData = `${line}\n`
+          const symbol = line.split(/[\s]+/)[1];
+          curSym = symbol;
+          curData = `${line}\n`;
         }
       }
       // sometimes set and parameters are displayed right below each other,
       // therefore the above if statement won't catch these statements
       else if (line.includes('PARAMETER') || line.includes('SET')) {
-        if (curSym) save(curSolve, curSym, curData)
+        if (curSym) save(curSolve, curSym, curData);
         if (curSection === 'Displays') {
-          const symbol = line.split(/[\s]+/)[2]
-          const pieces = line.split(/[\s]+/)
-          pieces.splice(0, 1)
-          const data = pieces.join(' ')
-          curSym = symbol
-          curData = `---- ${data}`
+          const symbol = line.split(/[\s]+/)[2];
+          const pieces = line.split(/[\s]+/);
+          pieces.splice(0, 1);
+          const data = pieces.join(' ');
+          curSym = symbol;
+          curData = `---- ${data}`;
         }
       }
       // abort if compilation errors
       else if (line.includes('Error Messages') || line.includes('**** USER ERROR(S) ENCOUNTERED')) {
         // rl.close()
         // stream.destroy()
-        console.log('User errors in DMP lst file', lst);
-        foundError = true
-        reject("User error in DMP .lst file")
+        console.warn('User errors in DMP lst file', lst);
+        foundError = true;
+        reject("User error in DMP .lst file");
       }
       // anything else
       else if (/^(\*\*\*\*)\s*\d/.test(line) || /^(GAMS)/.test(line) || line.includes("G e n e r a l") || /^(Range Statistics)/.test(line) || /^(RANGE STATISTICS)/.test(line) ) {
-        save(curSolve, curSym, curData)
+        save(curSolve, curSym, curData);
       } else {
-        curData += line + '\n'
+        curData += line + '\n';
       }
-    })
+    });
 
     rl.on('close', () => {
-      if (foundError) return
-      console.log('close and unlink', lst);
+      if (foundError) return;
       fs.unlink(lst, (err) => {
-        if (err) throw err
-      })
-      resolve()
-    })
+        if (err) throw err;
+      });
+      resolve();
+    });
 
-    rl.on('error', reject)
-  })
+    rl.on('error', reject);
+  });
 }
--- src/utils/createRefTree.js
+++ src/utils/createRefTree.js
@@ -1,37 +1,37 @@
-const readline = require('readline')
-const fs = require('fs')
-const path = require('path')
-const _ = require('lodash')
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
 
 module.exports = function createRefTree(refFile) {
   return new Promise((resolve, reject) => {
     const rl = readline.createInterface({
       input: fs.createReadStream(refFile),
       crlfDelay: Infinity
-    })
+    });
 
     // the ctags compatible .tags file
     // const tagsFile = fs.openSync(`${rootDir}${path.sep}.tags`, 'w')
     // the variable where we will store our reference tree
-    let json = {}
+    let json = {};
 
     rl.on('line', (line) => {
-      const segments = line.split(' ')
+      const segments = line.split(' ');
 
-      if (!isNaN(segments[1])) {
+      if (!isNaN(+segments[1])) {
         if (segments[4] === 'declared' || segments[4] === 'defined') {
-          let symbol = json[Number(segments[1])] = json[Number(segments[1])] || {}
+          let symbol = json[Number(segments[1])] = json[Number(segments[1])] || {};
 
-          let file
-          let base
+          let file;
+          let base;
           if (typeof segments[10] === 'string') {
-            file = segments.slice(10).join(' ')
-            base = path.parse(file).name
+            file = segments.slice(10).join(' ');
+            base = path.parse(file).name;
           }
           // make case-insensitive comparison possible (as GAMS is case-insensitive)
-          let nameLo
+          let nameLo;
           if (typeof segments[2] === 'string') {
-            nameLo = segments[2].toLowerCase()
+            nameLo = segments[2].toLowerCase();
           }
 
           Object.assign(symbol, {
@@ -43,7 +43,7 @@ module.exports = function createRefTree(refFile) {
               'file': file,
               'base': base
             }
-          })
+          });
 
           // save symbol to ctags file
           /*
@@ -56,14 +56,14 @@ module.exports = function createRefTree(refFile) {
           */
 
         } else {
-          json[segments[1]] = json[segments[1]] || {}
-          let action = json[segments[1]][segments[4]] = json[segments[1]][segments[4]] || []
+          json[segments[1]] = json[segments[1]] || {};
+          let action = json[segments[1]][segments[4]] = json[segments[1]][segments[4]] || [];
 
-          let file
-          let base
+          let file;
+          let base;
           if (typeof segments[10] === 'string') {
-            file = segments.slice(10).join(' ')
-            base = path.parse(file).name
+            file = segments.slice(10).join(' ');
+            base = path.parse(file).name;
           }
 
           action.push({
@@ -71,49 +71,61 @@ module.exports = function createRefTree(refFile) {
             'column': Number(segments[7]),
             'file': file,
             'base': base
-          })
+          });
         }
       } else if (segments[0] === '0') {
-        return
+        return;
       } else {
-        let symbol = json[segments[0]] = json[segments[0]] || {}
+        let symbol = json[segments[0]] = json[segments[0]] || {};
+        const domainCount = Number(segments[4]);
+        let domain = [];
+        if (domainCount > 0) {
+          const range = segments.slice(6, 6 + domainCount);
+          // '0' represents universal set
+          domain = range.map((entry) => {
+            if (entry !== '0' && json[entry].name !== symbol.name) return json[entry];
+            else return { name: '*' };
+          });
+        }
+        
+        let description = "";
+        if (domainCount > 0) {
+          description = segments.slice(6 + domainCount).join(' ');
+        };
 
         Object.assign(symbol, {
           'symId': segments[2],
           'type': segments[3],
-          get domain() {
-            const domainCount = Number(segments[4])
-            if (domainCount === 0) return null
-            const range = segments.slice(6, 6 + domainCount)
-            // '0' represents universal set
-            return range.map((entry) => {
-              if (entry !== '0') return json[entry]
-              else return { name: '*' }
-            })
-          },
-          get description() {
-            const domainCount = Number(segments[4])
-            if (domainCount === 0) return null
-            return segments.slice(6 + domainCount).join(' ')
-          }
-        })
+          'domain': domain,
+          'description': description
+        });
 
-        if (Number(segments[2]) === 2 && symbol.domain && symbol.domain.length === 1) {
+        if (Number(segments[2]) === 2 && symbol.domain && symbol.domain.length === 1 && symbol.domain[0].name !== '*') {
           Object.assign(symbol, {
-            'subset': true,
+            'isSubset': true,
             'superset': symbol.domain[0]
-          })
+          });
+          // add this set to the list of subsets of the superset
+          addSubsetToSupersetRecursive(symbol, symbol.superset);
         }
       }
-    })
+    });
+
+    function addSubsetToSupersetRecursive(subset, superset) {
+      superset.subsets = superset.subsets || [];
+      superset.subsets.push(subset);
+      if (superset.isSubset) {
+        addSubsetToSupersetRecursive(subset, superset.superset);
+      }
+    }
 
     rl.on('close', () => {
       fs.unlink(refFile, (err) => {
-        if (err) throw err
-      })
-      resolve(_.values(json))
-    })
+        if (err) throw err;
+      });
+      resolve(_.values(json));
+    });
 
-    rl.on('error', reject)
-  })
-}
+    rl.on('error', reject);
+  });
+};
